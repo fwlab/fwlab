12a08413160d35d8427dfd07058b853ee508c0f8
tengge1 <930372551@qq.com>
2021-12-13 19:23:24 +0800

    三角形

---
+++ src/scene/object/triangle.cpp
@@ -0,0 +1,59 @@
+#include <cmath>
+#include <filament/RenderableManager.h>
+#include <utils/EntityManager.h>
+#include "triangle.h"
+
+using namespace filament;
+
+struct Vertex {
+	math::float2 position;
+	uint32_t color;
+};
+
+static const Vertex TRIANGLE_VERTICES[3] = {
+	{{1, 0}, 0xffff0000u},
+	{{std::cos(M_PI * 2 / 3), std::sin(M_PI * 2 / 3)}, 0xff00ff00u},
+	{{std::cos(M_PI * 4 / 3), std::sin(M_PI * 4 / 3)}, 0xff0000ffu},
+};
+
+static constexpr uint16_t TRIANGLE_INDICES[3] = { 0, 1, 2 };
+
+Triangle::Triangle(Engine* engine, filament::Material* material)
+{
+	this->engine = engine;
+	this->material = material;
+
+	vertexBuffer = VertexBuffer::Builder()
+		.vertexCount(3)
+		.bufferCount(1)
+		.attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT2, 0, 12)
+		.attribute(VertexAttribute::COLOR, 0, VertexBuffer::AttributeType::UBYTE4, 8, 12)
+		.normalized(VertexAttribute::COLOR)
+		.build(*engine);
+	vertexBuffer->setBufferAt(*engine, 0,
+		VertexBuffer::BufferDescriptor(TRIANGLE_VERTICES, 36, nullptr));
+	indexBuffer = IndexBuffer::Builder()
+		.indexCount(3)
+		.bufferType(IndexBuffer::IndexType::USHORT)
+		.build(*engine);
+	indexBuffer->setBuffer(*engine,
+		IndexBuffer::BufferDescriptor(TRIANGLE_INDICES, 6, nullptr));
+
+	entity = utils::EntityManager::get().create();
+	RenderableManager::Builder(1)
+		.boundingBox({ { -1, -1, -1 }, { 1, 1, 1 } })
+		.material(0, material->getDefaultInstance())
+		.geometry(0, RenderableManager::PrimitiveType::TRIANGLES, vertexBuffer, indexBuffer, 0, 3)
+		.culling(false)
+		.receiveShadows(false)
+		.castShadows(false)
+		.build(*engine, entity);
+}
+
+Triangle::~Triangle()
+{
+	engine->destroy(entity);
+	engine->destroy(material);
+	engine->destroy(vertexBuffer);
+	engine->destroy(indexBuffer);
+}
--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -6,20 +6,22 @@
 #include <camutils/Manipulator.h>
 #include <viewer/SimpleViewer.h>
 #include <utils/EntityManager.h>
+#include "object/triangle.h"
 #include "resources/resources.h"
 #include "scene.h"
 
 filament::Skybox* skybox;
 filament::Material* mat;
-utils::Entity cube;
+Triangle* triangle;
 utils::Entity camera;
 
 void Scene::setup(filament::Engine* engine, filament::View* view, filament::Scene* scene)
 {
 	skybox = filament::Skybox::Builder()
-		.color({ 0.0, 0.0, 0.0, 1.0 })
+		.color({ 0.1, 0.125, 0.25, 1.0 })
 		.build(*engine);
 	scene->setSkybox(skybox);
+	view->setPostProcessingEnabled(false);
 
 	filament::viewer::SimpleViewer* viewer = new filament::viewer::SimpleViewer(engine, scene, view);
 
@@ -30,8 +32,8 @@ void Scene::setup(filament::Engine* engine, filament::View* view, filament::Scen
 		.package(RESOURCES_DEFAULTMATERIAL_DATA, RESOURCES_DEFAULTMATERIAL_SIZE)
 		.build(*engine);
 
-	cube = (new Cube(*engine, mat, { 1, 0, 0 }, false))->getSolidRenderable();
-	scene->addEntity(cube);
+	triangle = new Triangle(engine, mat);
+	scene->addEntity(triangle->entity);
 
 	camera = utils::EntityManager::get().create();
 	filament::Camera* cam = engine->createCamera(camera);
@@ -43,14 +45,14 @@ void Scene::setup(filament::Engine* engine, filament::View* view, filament::Scen
 void Scene::cleanup(filament::Engine* engine, filament::View* view, filament::Scene* scene)
 {
 	engine->destroy(skybox);
-	engine->destroy(cube);
 	engine->destroy(camera);
 	engine->destroyCameraComponent(camera);
 	utils::EntityManager::get().destroy(camera);
+	delete triangle;
 }
 
 void Scene::animate(filament::Engine* engine, filament::View* view, double now)
 {
-	auto& tcm = engine->getTransformManager();
-	tcm.setTransform(tcm.getInstance(cube), filament::math::mat4f::rotation(now, filament::math::float3{ 0, 0, 1 }));
+	//auto& tcm = engine->getTransformManager();
+	//tcm.setTransform(tcm.getInstance(cube), filament::math::mat4f::rotation(now, filament::math::float3{ 0, 0, 1 }));
 }
