9fbcbc3d51d7ef0058b5b2d7ce90f76880665221
tengge1 <930372551@qq.com>
2022-01-06 22:26:52 +0800

    update

--- src/gl/helper/GridHelper.h
+++ src/gl/helper/GridHelper.h
@@ -1,9 +1,10 @@
 #ifndef GL_HELPER_GRID_HELPER_H
 #define GL_HELPER_GRID_HELPER_H
 #include "../core/BufferGeometry.h"
+#include "../object/Mesh.h"
 #include "../material/StandardMaterial.h"
 
-class GridHelper
+class GridHelper : gl::object::Mesh
 {
 public:
 	GridHelper();
