2193b4cae811fd3fee9c4d8427051166d83ab9b5
liteng <930372551@qq.com>
2022-02-23 22:27:40 +0800

    兼容windows和ubuntu

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
+#ifdef WIN32
 #include <Windows.h>
+#endif
 #include "Application.h"
 
+#ifdef WIN32
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd
 )
+#else
+int main()
+#endif
 {
 	Application app;
 	app.start();
