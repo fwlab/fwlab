17b4f72f85a0298827e206ecd023fa7497fe36b0
tengge1 <930372551@qq.com>
2022-02-27 09:21:30 +0800

    test rapidjson

--- src/ui/menubar/FileMenu.cpp
+++ src/ui/menubar/FileMenu.cpp
@@ -1,5 +1,11 @@
+#include <fstream>
 #include <imgui.h>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
 #include "FileMenu.h"
+
+using namespace rapidjson;
 
 namespace fwlab::ui::menubar
 {
@@ -15,6 +21,24 @@ namespace fwlab::ui::menubar
 			}
 			if (ImGui::MenuItem("保存", "Ctrl+S", false, true))
 			{
+				// 1. 把 JSON 解析至 DOM。
+				const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+				Document d;
+				d.Parse(json);
+
+				// 2. 利用 DOM 作出修改。
+				Value& s = d["stars"];
+				s.SetInt(s.GetInt() + 1);
+
+				// 3. 把 DOM 转换（stringify）成 JSON。
+				StringBuffer buffer;
+				Writer<StringBuffer> writer(buffer);
+				d.Accept(writer);
+
+				// Output {"project":"rapidjson","stars":11}
+				std::ofstream out("test.txt");
+				out << buffer.GetString() << std::endl;
+				out.close();
 			}
 			if (ImGui::MenuItem("另存为...", nullptr, false, true))
 			{
