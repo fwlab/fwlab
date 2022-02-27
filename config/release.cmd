c751d566ac86c78d230a0c1a589802af82b92f8a
tengge1 <930372551@qq.com>
2022-02-27 08:57:49 +0800

    配置脚本

--- config/release.cmd
+++ config/release.cmd
@@ -1,5 +1,6 @@
 @echo off
 copy filament\release\CMakeSettings.json ..\third_party\filament\
+copy rapidjson\release\CMakeSettings.json ..\third_party\rapidjson\
 copy fwlab\release\CMakeSettings.json ..\
 echo success
 pause
