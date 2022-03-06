05b0f1325c7f7ed0cbf9f0ce17981bc3d4560c19
liteng <930372551@qq.com>
2022-03-06 20:38:58 +0800

    名称放到属性左边

--- src/ui/sidebar/property/ConfigPanel.cpp
+++ src/ui/sidebar/property/ConfigPanel.cpp
@@ -18,20 +18,37 @@ namespace fwlab::ui::sidebar::property
 		if (ImGui::CollapsingHeader("基本信息"))
 		{
 			char sceneName[20] = "场景";
-			ImGui::InputText("名称", sceneName, 20);
-			ImGui::InputText("类型", sceneName, 20);
+
+			ImGui::AlignTextToFramePadding();
+			ImGui::Text("名称"); ImGui::SameLine();
+			ImGui::InputText("", sceneName, 20);
+
+			ImGui::AlignTextToFramePadding();
+			ImGui::Text("类型"); ImGui::SameLine();
+			ImGui::InputText("", sceneName, 20);
 		}
 		if (ImGui::CollapsingHeader("位移组件"))
 		{
 			static float vec4a[4] = { 0.10f, 0.20f, 0.30f, 0.44f };
-			ImGui::InputFloat3("位置", vec4a);
-			ImGui::InputFloat3("旋转", vec4a);
-			ImGui::InputFloat3("缩放", vec4a);
+
+			ImGui::AlignTextToFramePadding();
+			ImGui::Text("位置"); ImGui::SameLine();
+			ImGui::InputFloat3("", vec4a);
+
+			ImGui::AlignTextToFramePadding();
+			ImGui::Text("旋转"); ImGui::SameLine();
+			ImGui::InputFloat3("", vec4a);
+
+			ImGui::AlignTextToFramePadding();
+			ImGui::Text("旋转"); ImGui::SameLine();
+			ImGui::InputFloat3("", vec4a);
 		}
 		if (ImGui::CollapsingHeader("场景组件"))
 		{
 			float col[3] = { 1, 0, 0 };
-			ImGui::ColorEdit3("背景", col);
+			ImGui::AlignTextToFramePadding();
+			ImGui::Text("背景"); ImGui::SameLine();
+			ImGui::ColorEdit3("", col);
 		}
 	}
 }
